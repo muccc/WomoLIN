46d769b278068cd8bfdcb776805f46ce83fa561f
Norym <myronfranze@web.de>
2019-11-01 09:27:53 +0100

    backup myfr stuff

--- Software/MyFr/signalsUnitsClasses/main.cpp
+++ Software/MyFr/signalsUnitsClasses/main.cpp
@@ -15,6 +15,54 @@ public:
 	virtual bool UpdateUnits(std::string & value) = 0;
 };
 
+
+template<class T>
+class Signal_Add_Unit
+{
+public:
+	Signal_Add_Unit() = default;
+	~Signal_Add_Unit() = default;
+
+	void AddUnit(T* unit);
+
+protected:
+	std::vector<T*> units;
+};
+
+template<class T>
+void Signal_Add_Unit<T>::AddUnit( T* unit )
+{
+	units.push_back(unit);
+}
+
+
+/*******************************************************************************
+ Signal CloseOpen
+*******************************************************************************/
+class I_Signal_CloseOpen
+{
+public:
+	virtual ~I_Signal_CloseOpen() = default;
+	virtual bool UpdateUnitSignalCloseOpen( std::string & signal_value ) = 0;
+};
+
+class Signal_CloseOpen final : public Signal_Add_Unit<I_Signal_CloseOpen>, public I_Signal
+{
+public:
+	virtual ~Signal_CloseOpen() = default;
+	bool UpdateUnits( std::string & signal_value ) override final;
+};
+
+bool Signal_CloseOpen::UpdateUnits(std::string & signal_value)
+{
+	for (const auto & unit : units){
+		unit->UpdateUnitSignalCloseOpen(signal_value);
+	}
+
+	return false;
+}
+
+
 /*******************************************************************************
  Signal OnOff
 *******************************************************************************/
@@ -22,36 +70,22 @@ class I_Signal_OnOff
 {
 public:
 	virtual ~I_Signal_OnOff() = default;
-
 	virtual bool UpdateUnitSignalOnOff( std::string & signal_value ) = 0;
-
 };
 
-class Signal_OnOff : public I_Signal
+class Signal_OnOff final : public Signal_Add_Unit<I_Signal_OnOff>,  public I_Signal
 {
 public:
 	virtual ~Signal_OnOff() = default;
-
-	void AddUnit(I_Signal_OnOff* unit);
 	bool UpdateUnits( std::string & signal_value ) override final;
-
-private:
-	std::vector<I_Signal_OnOff*> units;
 };
 
-
-
-void Signal_OnOff::AddUnit(I_Signal_OnOff* unit)
-{
-	units.push_back(unit);
-}
 
 bool Signal_OnOff::UpdateUnits(std::string & signal_value)
 {
 	for (const auto & unit : units){
 		unit->UpdateUnitSignalOnOff(signal_value);
 	}
-
 	return false;
 }
 
@@ -60,7 +94,7 @@ bool Signal_OnOff::UpdateUnits(std::string & signal_value)
  Unit BiRelay
 *******************************************************************************/
 
-class I_BiRelay : public I_Signal_OnOff
+class I_BiRelay : public I_Signal_OnOff, public I_Signal_CloseOpen
 {
 public:
 	~I_BiRelay() = default;
@@ -68,45 +102,60 @@ public:
 };
 
 
-class BiRelay : public I_BiRelay
+class BiRelay final : public I_BiRelay
 {
 public:
 	BiRelay() = default;
 	~BiRelay() = default;
 
 	bool UpdateUnitSignalOnOff( std::string & signal_value ) override final;
+	bool UpdateUnitSignalCloseOpen( std::string & signal_value ) override final;
 };
-
 
 bool BiRelay::UpdateUnitSignalOnOff( std::string & signal_value )
 {
 	return false;
 }
 
-/*
- * licht_an_aus, type=Signal_OnOff
- * licht_an_aus, unit=birelay_1
- */
+bool BiRelay::UpdateUnitSignalCloseOpen( std::string & signal_value )
+{
+	return false;
+}
+
+
 
 int main() {
    std::cout << "Hello Womo" << std::endl;
 
-   // kommt aus Lib, alle Units sind bekannt
+   // Objekte kommen aus Lib
    auto bi_relay1 = BiRelay();
+   auto bi_relay2 = BiRelay();
 
-   // Signale und Unitskommen aus dem XML
-   auto licht_an_aus = Signal_OnOff();
-   licht_an_aus.AddUnit(&bi_relay1);
-
+   // Signale und Units kommen aus dem XML
+   // anlegen des Signal Vectors
    std::map<std::string, I_Signal*> signal_vector;
+
+   auto licht_an_aus = Signal_OnOff();
    signal_vector["licht_an_aus"] = &licht_an_aus;
 
-   // Licht_an_aus = ON
-   auto signal = std::string("licht_an_aus");
-   auto signal_data = std::string("ON");
+   auto pumpe_an_aus = Signal_CloseOpen();
+   signal_vector["pumpe_an_aus"] = &pumpe_an_aus;
+
+   // Units zu den Signalen hinzufuegen
+
+   licht_an_aus.AddUnit(&bi_relay1);
+   pumpe_an_aus.AddUnit(&bi_relay2);
+
+   // signale verarbeiten
+
+   auto sig_licht_an_aus = std::string("licht_an_aus");
+   auto sig_licht_an_aus_value = std::string("ON");
+   signal_vector[sig_licht_an_aus]->UpdateUnits(sig_licht_an_aus_value);
 
 
-   signal_vector[signal]->UpdateUnits(signal_data);
+   auto sig_pumpe_an_aus = std::string("pumpe_an_aus");
+   auto sig_pumpe_an_aus_value = std::string("CLOSE");
+   signal_vector[sig_pumpe_an_aus]->UpdateUnits(sig_pumpe_an_aus_value);
 
    return 0;
 }
