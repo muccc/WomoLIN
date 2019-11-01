81692d2fb7160a12df157615d387f7018c05a354
Norym <myronfranze@web.de>
2019-11-01 08:38:51 +0100

    backup myfr stuff

--- Software/MyFr/signalsUnitsClasses/main.cpp
+++ Software/MyFr/signalsUnitsClasses/main.cpp
@@ -1,88 +1,112 @@
 #include <iostream>
 #include <vector>
 #include <memory>
+#include <string>
+#include <tuple>
+#include <functional>
+#include <map>
 
-/*******************************************************************************
- SignalOnOff
-*******************************************************************************/
-class I_Signal_Input_OnOff
+
+
+class I_Signal
 {
 public:
-   virtual ~I_Signal_Input_OnOff() = default;
-   virtual bool setSignalOnOff(const std::string & signal_value) = 0;       
-};
-
-
-/*******************************************************************************
- Units Main Unit Interfaces
-*******************************************************************************/
-
-class I_Unit_Output_BiRelay : public I_Signal_Input_OnOff
-{
-public:
-	virtual ~I_Unit_Output_BiRelay() = default;
-	bool setSignalOnOff(const std::string & signal_value) override = 0;
-
+	virtual ~I_Signal() = default;
+	virtual bool UpdateUnits(std::string & value) = 0;
 };
 
 /*******************************************************************************
- Units Main Unit 
+ Signal OnOff
 *******************************************************************************/
-
-class Unit_Output_BiRelay : public I_Unit_Output_BiRelay
+class I_Signal_OnOff
 {
 public:
-   ~Unit_Output_BiRelay() = default;
-   bool setSignalOnOff(const std::string & signal_value) override final { std::cout << "setSignalOnOff" << std::endl; return true; };       
+	virtual ~I_Signal_OnOff() = default;
+
+	virtual bool UpdateUnitSignalOnOff( std::string & signal_value ) = 0;
+
 };
 
-
-/*******************************************************************************
- Input Signals 
-*******************************************************************************/
-
-template<class T>
-class Signal_Input
+class Signal_OnOff : public I_Signal
 {
 public:
-   ~Signal_Input() = default;
-   Signal_Input(std::vector<T *> & units) : units(units) {};
-   void SendToUnits() {};
+	virtual ~Signal_OnOff() = default;
+
+	void AddUnit(I_Signal_OnOff* unit);
+	bool UpdateUnits( std::string & signal_value ) override final;
+
 private:
-   std::vector<T *>&  units;
-};
-
-template<>
-void Signal_Input<I_Signal_Input_OnOff>::SendToUnits()
-{ 
-   for( auto const &unit : units )
-   {
-      unit->setSignalOnOff(""); 
-   }
+	std::vector<I_Signal_OnOff*> units;
 };
 
 
-int main(){
-   std::cout << "Hello WomoLIN" << std::endl;
 
-   // create all units
-   auto biRelay1 = Unit_Output_BiRelay();
+void Signal_OnOff::AddUnit(I_Signal_OnOff* unit)
+{
+	units.push_back(unit);
+}
 
+bool Signal_OnOff::UpdateUnits(std::string & signal_value)
+{
+	for (const auto & unit : units){
+		unit->UpdateUnitSignalOnOff(signal_value);
+	}
 
-   // create Unit vectors for input signals
-   std::vector<I_Signal_Input_OnOff *>        TestInputSignalOnOff1_Units;
-
-
-   // add units to input signals 
-   TestInputSignalOnOff1_Units.push_back( &biRelay1 );
-
-
-   // create input signals and inject the units
-   auto TestInputSignalOnOff1 = Signal_Input<I_Signal_Input_OnOff>(TestInputSignalOnOff1_Units);
+	return false;
+}
 
 
-   TestInputSignalOnOff1.SendToUnits();
+/*******************************************************************************
+ Unit BiRelay
+*******************************************************************************/
 
+class I_BiRelay : public I_Signal_OnOff
+{
+public:
+	~I_BiRelay() = default;
+	bool UpdateUnitSignalOnOff( std::string & signal_value ) = 0;
+};
+
+
+class BiRelay : public I_BiRelay
+{
+public:
+	BiRelay() = default;
+	~BiRelay() = default;
+
+	bool UpdateUnitSignalOnOff( std::string & signal_value ) override final;
+};
+
+
+bool BiRelay::UpdateUnitSignalOnOff( std::string & signal_value )
+{
+	return false;
+}
+
+/*
+ * licht_an_aus, type=Signal_OnOff
+ * licht_an_aus, unit=birelay_1
+ */
+
+int main() {
+   std::cout << "Hello Womo" << std::endl;
+
+   // kommt aus Lib, alle Units sind bekannt
+   auto bi_relay1 = BiRelay();
+
+   // Signale und Unitskommen aus dem XML
+   auto licht_an_aus = Signal_OnOff();
+   licht_an_aus.AddUnit(&bi_relay1);
+
+   std::map<std::string, I_Signal*> signal_vector;
+   signal_vector["licht_an_aus"] = &licht_an_aus;
+
+   // Licht_an_aus = ON
+   auto signal = std::string("licht_an_aus");
+   auto signal_data = std::string("ON");
+
+
+   signal_vector[signal]->UpdateUnits(signal_data);
 
    return 0;
 }
