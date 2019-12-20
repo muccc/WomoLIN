e4201d6da01e66a2ff10b186254ab06844e1e0e5
Norym <myronfranze@web.de>
2019-12-20 15:33:22 +0000

    backup getgitinfos.sh

--- Software/WomoLin/getgitinfos.sh
+++ Software/WomoLin/getgitinfos.sh
@@ -2,6 +2,16 @@
 
 if [[ `git status --short` ]]; then
    echo "git hat aenderungen"
+   GIT_HASH="NO_HASH"
+   RELEASE="LOCAL"
 else
    echo "git hat keine aenderungen"
+   GIT_HASH=`git rev-parse --short HEAD`
+   RELEASE="RELEASE"
 fi
+
+DATE=`date`
+
+echo ${GIT_HASH}
+echo ${RELEASE}
+echo ${DATE}
