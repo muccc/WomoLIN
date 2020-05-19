db42470b2d8ebcc6d71a6e43eb5a97b822692d6f
Tilo Seeck <tiloseeck@web.de>
2020-05-19 12:21:32 +0200

    Add driver hal cmsis libs to main target.

--- Software/firmware/target/mainunit/hwinit.cpp
+++ Software/firmware/target/mainunit/hwinit.cpp
@@ -3,10 +3,17 @@
 /* Author Myron Franze <myronfranze@web.de> */
 
 #include "include/hwinit.h"
+#include "drv/inc/hal.hpp"
+
+UART_HandleTypeDef huart1;
+UART_HandleTypeDef huart2;
+SPI_HandleTypeDef hspi3;
 
 namespace womolin::target
 {
    void HwInit()
    {
+	   HAL hHal;
+	   hHal.init();
    } 
 }
